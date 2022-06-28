36bdfe716cebd4ab8b92eba573bd3fe20936eb9d
Masturbator2000 <107932638+Masturbator2000@users.noreply.github.com>
2022-06-28 18:38:36 +0300

    main

---
+++ main.py
@@ -0,0 +1,36 @@
+# Самый простой скрипт, чтобы напугать человека.
+
+# ! Создано в развлекательных целях.
+# ! Всю ответственность вы перекладываете на себя.
+
+# * Информация
+# Скрипт не добавляется в автозапуск
+# Автоматически устанавливается громкость 100%
+
+# * Если подкинуть однокласснику на уроке информатики, вообще ржачка будет.
+# * всем советую.
+
+# * P.S Вы можете скачать EXE файл в репозитории, и не собирать его при помощи pyinstaller'a.
+
+# 1. Модули
+
+# pip install simpleaudio
+from simpleaudio import WaveObject
+
+# pip install keyboard
+import keyboard
+
+# 2. Устанавливаем громкость 100%
+for x in range(50):
+    keyboard.press('volume up')
+
+# 3. Блокируем все клавиши
+for x in range(1, 150):
+    keyboard.block_key(x)
+
+# 4. Играем звук (бесконечно)
+wave_obj = WaveObject.from_wave_file("./ScreamerSound.wav")
+
+while True:
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
